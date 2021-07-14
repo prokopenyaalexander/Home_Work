4556f7885cbb529c53deecae618f2be7203a5f22
Alexander <47172988+prokopenyaalexander@users.noreply.github.com>
2021-07-15 02:11:11 +0300

    Delete hw 8.1.py

--- hw 8.1.py
+++
@@ -1,9 +0,0 @@
-list_str = ['str', 'буква','Машина','имя','555']
-list_str.sort()
-i = 0
-#for value in list_str:
-    
-    #print(i,value)
-    #i += 1
-sorted_list = [(i.index(list_str),value) for value in list_str]
-print(sorted_list)
